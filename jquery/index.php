3e5be7c4e2c126514b92b26654859286692a1a24
Marceau Rodrigues <marceau0707@gmail.com>
2023-01-23 22:17:17 +0100

    Ajout de la gestion via jQuery

--- jquery/index.php
+++ jquery/index.php
@@ -19,6 +19,7 @@
         <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
         <script>
             function changerDonnees() {
+                event.preventDefault();
                 $.ajax({
                     url: "../data.php", 
                     method: "post",
